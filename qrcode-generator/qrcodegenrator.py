b702db63ae8e72a20e94065649d289698e0cf19b
ThatOneKrish <csrealracing13d@gmail.com>
2022-09-02 14:36:35 +0530

    Add files via upload

---
+++ qrcode-generator/qrcodegenrator.py
@@ -0,0 +1,18 @@
+import qrcode
+
+def generate_qrcode(text):
+
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrcode.png")
+
+url = input("Enter your Website URL: ")
+generate_qrcode(url)
