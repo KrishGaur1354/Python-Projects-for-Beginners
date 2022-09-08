081b62bda686a3a49cc1168072661336e53bbdf1
ThatOneKrish <csrealracing13d@gmail.com>
2022-09-08 19:54:41 +0530

    Update rainbowhex.py

--- Rainbow-Hex-Pattern/rainbowhex.py
+++ Rainbow-Hex-Pattern/rainbowhex.py
@@ -4,6 +4,7 @@ colors=["red" , "orange" , "yellow" , "green" , "blue" ,"indigo" , "violet" ]
 t=turtle.Pen()
 turtle.bgcolor("black")
 t.speed(0)
+
 for x in range(360):
     t.pencolor(colors[x%7])
     t.width(x//100+1)
