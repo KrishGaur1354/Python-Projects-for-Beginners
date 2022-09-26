24d5c78afb556f4cc2fef8571f1580da05202bd6
ThatOneKrish <csrealracing13d@gmail.com>
2022-09-26 12:57:38 +0530

    Delete geoloco.py
    Signed-off-by: ThatOneKrish <csrealracing13d@gmail.com>

--- geoloco.py
+++
@@ -1,8 +0,0 @@
-from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="geoapiExercises") 
-a = input("Enter the zipcode : ")
-zipcode = a 
-location = geolocator.geocode(zipcode) 
-print("Zipcode:",zipcode)
-print("Details of the Zipcode:")
-print(location)  
